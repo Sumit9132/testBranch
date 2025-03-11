cd626d58911e70bafa3f2b80e08a1fe819472f7d
Sumit9132 <sumitguptazone01@gmail.com>
2025-03-11 22:10:03 +0530

    first message

---
+++ src/test/java/com/testBranch/TestBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testBranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
